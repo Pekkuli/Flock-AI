677b8caee1b65f405a855006c76d8b94fef6ab05
Pekkuli <anssi.kivinen@gmail.com>
2023-12-20 00:24:07 +0200

    Fine tuning
    -Removed unnecessary code / general cleaning
    -Added trails for birb/predators
    -Fixed predators avoiding predators
    -Adjusted default values

--- flock.js
+++ flock.js
@@ -1,18 +1,18 @@
 //Simulation parameters
 let width = 300;
 let height = 300;
-let sim_boundary = 50;
+let sim_boundary = 75;
 var stop = false;
 var interval = 1000 / 60;  //   (ms per second)/(wanted fps) --> framerate is either fps or screen refresh rate which ever is smaller
 var lastFrame, then, elapsed;
-var showRange, showTrail = false;
+var showRange = false, showTrail = true;
 let numbBirbs = 150;
-let numPredators = 1;
+let numPredators = 3;
 
 //Birb parameters
-let visualRange = 100;
+let visualRange = 75;
 let max_speed = 10;
-let turn_speed = 5;
+let turn_speed = 2;
 let min_distance = 25;
 let flocking_factor = 0.01;
 let matching_factor = 0.05;
@@ -20,10 +20,11 @@ let avoid_factor = 0.05;
 
 //Predator parameters
 let predator_visualRange = 100;
-let predator_max_speed = 11;
-let chasing_factor = 0.1;
+let predator_max_speed = 9.5;
+let chasing_factor = 0.15;
 let predator_eat_range = 10;
-let predator_min_distance = 10;
+let predator_min_distance = 25;
+let predator_avoid_factor = 0.1;
 
 var firstClick = [null,null];
 var globalFlock = [];
@@ -38,77 +39,8 @@ function resizeCanvas() {
     canvas.height = window.innerHeight;
 }
 
-// function calcDist(x1, y1, x2, y2) {
-//     let a = x1 - x2;
-//     let b = y1 - y2;
-//     return Math.sqrt( a*a + b*b );
-// }
-
-// function getCords(event) {
-//     var eventDoc, doc, body;
-    
-//     event = event || window.event;
-    
-//     if (event.pageX == null && event.clientX != null) {
-//     eventDoc = (event.target && event.target.ownerDocument) || document;
-//     doc = eventDoc.documentElement;
-//     body = eventDoc.body;
-
-//     event.pageX = event.clientX +
-//         (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-//         (doc && doc.clientLeft || body && body.clientLeft || 0);
-//     event.pageY = event.clientY +
-//         (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
-//         (doc && doc.clientTop  || body && body.clientTop  || 0 );
-//     }
-//     return [event.pageX, event.pageY] 
-// }
-
-/* function handleMouseMove(event) {
-    var cords = getCords(event);
-    x = cords[0]
-    y = cords[1]
-
-    spawnBirb(x, y);
-} */
-
-function handleMouseClick(event, amount) {
-    let cords = getCords(event);
-    let x = cords[0]
-    let y = cords[1]
-    this.firstClick = [x,y]
-    
-    let targets = document.getElementsByClassName("target")
-    while(targets[0]) {
-        targets[0].parentNode.removeChild(targets[0]);
-    }
-    
-    let target = document.createElement('div');
-    target.className = "target";
-    target.style.left = x + "px";
-    target.style.top = y + "px";
-    document.body.appendChild(target);
-    console.log(`Target coordinates: (${x},${y})!`)
-
-    startSim(amount)
-    /* if(firstClick[0] == null && firstClick[1] == null) {
-        firstClick = [x,y]
-        console.log("first click:  ("+firstClick+")")
-    } else if(secondClick[0] == null && secondClick[1] == null) {
-        secondClick = [x,y]
-        console.log("second click: ("+secondClick+")")
-        console.log("Distance:     ("+calcDist(firstClick[0],firstClick[1],x,y)+")")
-        drawLine(firstClick[0],firstClick[1],x,y)
-    } else {
-        firstClick = secondClick
-        secondClick = [x,y]
-        console.log("new click:    ("+firstClick+")\n              ("+secondClick+")")
-        console.log("Distance:     ("+calcDist(firstClick[0],firstClick[1],x,y)+")")
-        drawLine(firstClick[0],firstClick[1],x,y)
-    } */
-}
-
 function calcDist(birb1, birb2) {
+    // pythagoras
     let a = birb1.x - birb2.x;
     let b = birb1.y - birb2.y;
     return Math.sqrt( a*a + b*b );
@@ -150,26 +82,6 @@ function getLocalFlock(birb, flock) {
 }
 
 function flockWithOthers(flock, birb) {
-
-    // let avg_x = 0;
-    // let avg_y = 0;
-    // let localFlockSize = 0;
-
-    // for(let otherBirb of globalFlock) {
-    //     if (calcDist(birb, otherBirb) < visualRange) {
-    //         avg_x += otherBirb.x;
-    //         avg_y += otherBirb.y;
-    //         localFlockSize++;
-    //     }
-    // }
-
-    // if (localFlockSize) {
-    //     avg_x = avg_x / localFlockSize;
-    //     avg_y = avg_y / localFlockSize;
-
-    //     birb.dx += (avg_x - birb.x) * flocking_factor;
-    //     birb.dy += (avg_y - birb.y) * flocking_factor;
-    // }
     if (flock.size) {
         birb.dx += (flock.x - birb.x) * flocking_factor;
         birb.dy += (flock.y - birb.y) * flocking_factor;
@@ -178,32 +90,13 @@ function flockWithOthers(flock, birb) {
 }
 
 function avoidOthers(flock, birb) {
-    if (flock.size) {
-        birb.dx += flock.avoid_x * avoid_factor;
-        birb.dy += flock.avoid_y * avoid_factor;
+    if (flock.size > 1) {
+        birb.dx += flock.avoid_x * birb.avoid_factor;
+        birb.dy += flock.avoid_y * birb.avoid_factor;
     }
 }
 
 function matchSpeed(flock, birb) {
-    // let avg_dx = 0;
-    // let avg_dy = 0;
-    // let localFlockSize = 0;
-
-    // for(let otherBirb of globalFlock) {
-    //     if (calcDist(Birb, otherBirb) < visualRange) {
-    //         avg_dx += otherBirb.dx;
-    //         avg_dy += otherBirb.dy;
-    //         localFlockSize++;
-    //     }
-    // }
-
-    // if (localFlockSize) {
-    //     avg_dx = avg_dx / localFlockSize;
-    //     avg_dy = avg_dy / localFlockSize;
-
-    //     Birb.dx += (avg_dx - Birb.dx) * matching_factor;
-    //     Birb.dy += (avg_dy - Birb.dy) * matching_factor;
-    // }
     if (flock.size) {
         birb.dx += (flock.dx - birb.dx) * matching_factor;
         birb.dy += (flock.dy - birb.dy) * matching_factor;
@@ -217,9 +110,6 @@ function avoidPredators(birb) {
             birb.dy -= (predator.y - birb.y) * avoid_factor;
         }
     }
-
-    // birb.dx -= (flock.x - birb.x) * flocking_factor;
-    // birb.dy -= (flock.y - birb.y) * flocking_factor;
 }
 
 function limitSpeed(birb) {
@@ -262,28 +152,17 @@ function keepInsideBoundary(Birb) {
     }
 }
 
-
-// function spawnBirb(x, y) {
-//     let Birb = document.createElement('div');
-//     Birb.className = "Birb";
-//     Birb.style.left = x + "px";
-//     Birb.style.top = y + "px";
-//     document.body.appendChild(Birb);
-// }
-
 function spawnBirbs() {
     console.log(`Spawning Birbs! (${numbBirbs})`)
 
     for (let i = 0; i < numbBirbs;i++){
-        /* let x = Math.floor(Math.random() * width)
-        let y = Math.floor(Math.random() * height)
-        spawnBirb(x, y) */
         globalFlock[globalFlock.length] = {
             x: Math.random() * width,
             y: Math.random() * height,
             dx: Math.random() * 2 * max_speed - max_speed,
             dy: Math.random() * 2 * max_speed - max_speed,
             max_speed: max_speed,
+            avoid_factor: avoid_factor,
             min_distance: min_distance,
             visual_range: visualRange,
             trail: [],
@@ -301,6 +180,7 @@ function spawnPredators() {
                 dx: Math.random() * 2 * predator_max_speed - predator_max_speed,
                 dy: Math.random() * 2 * predator_max_speed - predator_max_speed,
                 max_speed: predator_max_speed,
+                avoid_factor: predator_avoid_factor,
                 min_distance: predator_min_distance,
                 visual_range: predator_visualRange,
                 trail: [],
@@ -318,21 +198,8 @@ function chaseBirbs(predator) {
                 closestBirb = birb;
                 dist = calcDist(predator, birb);
                 if (dist <= predator_eat_range) {
-                    // console.log(`Birb got eaten at (${(closestBirb.x).toFixed(1)},${(closestBirb.y).toFixed(1)})!`)
                     globalFlock.splice(globalFlock.indexOf(closestBirb),1);
                 }
-                // avg_x += birb.x;
-                // avg_dx += birb.dx;
-                // avg_y += birb.y;
-                // avg_dy += birb.dy;
-                // if other birbs inside avoid sphere, get sum of the difference in position
-                // if(birb != birb) {
-                //     if(calcDist(birb, birb) < min_distance) {
-                //         avoid_x += birb.x - birb.x;
-                //         avoid_y += birb.y - birb.y;
-                //     }
-                // }
-                // localFlockSize++;
             }
         }
     }
@@ -342,48 +209,7 @@ function chaseBirbs(predator) {
     }
 }
 
-/* function advanceBirbs() {
-    let Birbs = document.getElementsByClassName("Birb")
-
-    if(Birbs.length == 0) {
-        this.stop = true;
-        console.log("stopping timer!");
-    } else {
-        for (let Birb of Birbs) {
-            creepOnClick(Birb)
-        }
-    }
-} */
-
-/* function creepOnClick(Birb) {
-    let targetX = this.firstClick[0]
-    let targetY = this.firstClick[1]
-
-    let x = parseFloat(Birb.style.left)
-    let y = parseFloat(Birb.style.top)
-
-    // vektori
-    let dx = targetX - x;
-    let dy = targetY - y;
-    //normalisoi
-    let l = Math.sqrt( dx*dx + dy*dy)
-    let dnx = (dx/l);
-    let dny = (dy/l);
-
-    Birb.style.left = (x + dnx) + "px";
-    Birb.style.top = (y + dny) + "px";
-
-    if (((Math.abs(targetX-x) < 1) && (Math.abs(targetY-y) < 1))) {
-        Birb.remove();
-    }
-} */
-
 function startSim() {
-    // spawnBirbs(amount)
-    // if(!this.timer) {
-    //     console.log(`Starting simulation!`)
-    //     this.timer = setInterval(advanceBirbs, interval);
-    // }
     this.stop = false;
     then = window.performance.now();
     spawnBirbs()
@@ -393,14 +219,11 @@ function startSim() {
 
 function drawBirb(ctx, birb) {
     const a = Math.atan2(birb.dy, birb.dx);
-
     //Moving / rotating canvas with birb in the center 
     ctx.translate(birb.x, birb.y);
     ctx.rotate(a);
     ctx.translate(-birb.x, -birb.y);
-    
     //Drawing the birb
-    //ctx.fillStyle = "#978832";
     ctx.fillStyle = "#e6c814";
     ctx.strokeStyle = "Black";
     ctx.beginPath();
@@ -412,6 +235,7 @@ function drawBirb(ctx, birb) {
     ctx.lineWidth = 1;
     ctx.stroke();
     ctx.closePath();
+    // draw FOV circle if enabled
     if(showRange) {
         ctx.beginPath();
         ctx.strokeStyle = "Black";
@@ -419,9 +243,18 @@ function drawBirb(ctx, birb) {
         ctx.stroke();
         ctx.closePath();
     }
-
     //Reseting canvas
     ctx.setTransform(1, 0, 0, 1, 0, 0);
+    // draw trail if enabled
+    if (showTrail) {
+        ctx.strokeStyle = "#e6c864";
+        ctx.beginPath();
+        ctx.moveTo(birb.trail[0][0], birb.trail[0][1]);
+        for(var i=0, len=birb.trail.length; i<len; ++i) {
+            ctx.lineTo(birb.trail[i][0], birb.trail[i][1]);
+        } 
+        ctx.stroke(); 
+    }
 }
 
 function drawBirbs(ctx) {
@@ -440,7 +273,6 @@ function drawPredator(ctx, predator) {
     ctx.fillStyle = "#d1561d";
     ctx.strokeStyle = "Black";
     ctx.beginPath();
-    //ctx.moveTo(predator.x, predator.y);
     ctx.rect(predator.x-5, predator.y-5, 10, 10);
     ctx.fill();
     ctx.lineWidth = 1;
@@ -459,6 +291,15 @@ function drawPredator(ctx, predator) {
     }
     //Reseting canvas
     ctx.setTransform(1, 0, 0, 1, 0, 0);
+    if (showTrail) {
+        ctx.strokeStyle = "#ff8950";
+        ctx.beginPath();
+        ctx.moveTo(predator.trail[0][0], predator.trail[0][1]);
+        for(var i=0, len=predator.trail.length; i<len; ++i) {
+            ctx.lineTo(predator.trail[i][0], predator.trail[i][1]);
+        } 
+        ctx.stroke(); 
+    }
 }
 
 function drawPredators(ctx) {
@@ -508,11 +349,13 @@ function simulateLoop() {
             for (let predator of predator_pack) {
                 let pack = getLocalFlock(predator, predator_pack);
                 chaseBirbs(predator);
-                avoidOthers(predator, pack);
+                avoidOthers(pack, predator);
                 limitSpeed(predator);
                 keepInsideBoundary(predator);
                 predator.x += predator.dx;
                 predator.y += predator.dy;
+                predator.trail.push([predator.x, predator.y]);
+                predator.trail = predator.trail.slice(-50);
             }
         }
 
